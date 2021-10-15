fa911d073194c537663f1025c38e7bc82ba9d557
hosimiya7 <nerokarurin@gmail.com>
2021-10-16 00:47:22 +0900

    sp完成

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  ハンバーガーメニュー
 */
 
-$(function() {
-    $('.hamburger').click(function() {
+$(function () {
+    $('.hamburger').click(function () {
         $(this).toggleClass('active');
 
         if ($(this).hasClass('active')) {
@@ -18,38 +18,59 @@ $(function() {
  *  スライダー
  */
 
- $('.slider').slick({
+$('.slider').slick({
     infinite: true,
     speed: 1000,
     slidesToShow: 1,
     adaptiveHeight: true,
     autoplay: true,
-    autoplaySpeed:2500,
-    arrows:false,
-  });
+    autoplaySpeed: 2500,
+    arrows: false,
+});
 
-  $('.event-slider').slick({
+$('.event-slider').slick({
     infinite: true,
     speed: 1000,
     slidesToShow: 1,
     adaptiveHeight: true,
     autoplay: true,
-    autoplaySpeed:2500,
-    arrows:true,
-    prevArrow:'<div class="prev"><i class="fas fa-arrow-circle-left"></i></div>',
-	nextArrow:'<div class="next"><i class="fas fa-arrow-circle-right"></i></div>',
-  });
+    autoplaySpeed: 2500,
+    arrows: true,
+    prevArrow: '<div class="prev"><i class="fas fa-arrow-circle-left"></i></div>',
+    nextArrow: '<div class="next"><i class="fas fa-arrow-circle-right"></i></div>',
+});
 
-  $('.guide-slider').slick({
-    fade:true,
+$('.guide-slider').slick({
+    fade: true,
     infinite: true,
     speed: 1000,
     slidesToShow: 1,
     adaptiveHeight: true,
     autoplay: true,
-    autoplaySpeed:3000,
-    arrows:true,
-    prevArrow:'<div class="prev"><i class="fas fa-arrow-circle-left"></i></div>',
-	nextArrow:'<div class="next"><i class="fas fa-arrow-circle-right"></i></div>',
-  });
+    autoplaySpeed: 3000,
+    arrows: true,
+    prevArrow: '<div class="prev"><i class="fas fa-arrow-circle-left"></i></div>',
+    nextArrow: '<div class="next"><i class="fas fa-arrow-circle-right"></i></div>',
+});
+
+// 波紋
+$('.ripples').ripples({
+    resolution: 400,
+});
+
+// raindrops
+jQuery(".raindrops").raindrops({
+    color: "#2079bd",
+    canvasHeight: 60
+});
+
+// アコーディオンメニュー
+$(function () {
+    //.accordion_oneの中の.accordion_headerがクリックされたら
+    $('.s_07 .accordion_one .accordion_header').click(function () {
+        //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+        $(this).next('.accordion_inner').slideToggle();
+        $(this).toggleClass("open");
+    });
+});
 
